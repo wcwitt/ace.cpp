f62633924fc7be9b28e3c364cb144754153a44d9
Chuck Witt <witt.chuck@gmail.com>
2022-12-28 12:08:41 +0000

    add sph_harm_xyz to pybind.

--- source/pybind/ace-pybind.cpp
+++ source/pybind/ace-pybind.cpp
@@ -7,4 +7,5 @@ PYBIND11_MODULE(ace, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("naive_sph_harm", &naive_sph_harm, "A naive spherical harmonic.");
+    m.def("naive_sph_harm_xyz", &naive_sph_harm_xyz, "A naive spherical harmonic.");
 }
