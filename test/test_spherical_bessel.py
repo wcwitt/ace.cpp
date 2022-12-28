16233dbe7fc5610c60bf2bb89b81ffee474b7a42
Chuck Witt <witt.chuck@gmail.com>
2022-12-28 13:07:08 +0000

    add test for determine basis.

--- test/test_spherical_bessel.py
+++ test/test_spherical_bessel.py
@@ -7,7 +7,7 @@ from scipy.special import jn_zeros
 from scipy.special import spherical_jn
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../build/'))
-from ace import spherical_bessel_radial
+from ace import spherical_bessel_radial, determine_basis
 
 def test_spherical_bessel_radial():
 
@@ -29,3 +29,15 @@ def test_spherical_bessel_radial():
                 x1 = scipy_spherical_bessel_radial(n, l, r, r_c)
                 x2 = spherical_bessel_radial(n, l, r, r_c)
                 assert x1 == pytest.approx(x2)
+
+def test_determine_basis():
+
+    A_n, A_l, A_m, A_k = determine_basis(5.0, 1.0)
+    # TODO: verify that these are actually correct ... right now only
+    #       testing backwards compatibility.
+    assert A_n == [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert A_l == [0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3]
+    assert A_m == [0, 0, -1, 0, 1, -2, -1, 0, 1, 2, -3, -2, -1, 0, 1, 2, 3]
+    assert A_k == pytest.approx([0.628, 1.257, 0.899, 0.899, 0.899, 1.153,
+                                 1.153, 1.153, 1.153, 1.153, 1.398, 1.398,
+                                 1.398, 1.398, 1.398, 1.398, 1.398], abs=0.001)
