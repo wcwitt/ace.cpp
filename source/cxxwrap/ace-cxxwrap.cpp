c87509f9687d87f14d1a4ba5771b42afa2194c3a
Chuck Witt <witt.chuck@gmail.com>
2023-01-02 21:41:07 +0000

    add jl wrapper for simpleprodbasis.

--- source/cxxwrap/ace-cxxwrap.cpp
+++ source/cxxwrap/ace-cxxwrap.cpp
@@ -1,7 +1,10 @@
 #include "jlcxx/jlcxx.hpp"
+#include "jlcxx/stl.hpp"
 
 #include "spherical_harmonic.hpp"
 #include "spherical_bessel.hpp"
+
+#include "acecore/simpleprodbasis.hpp"
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& m)
 {
@@ -9,4 +12,6 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& m)
     m.method("naive_sph_harm_xyz", &naive_sph_harm_xyz);
     m.method("spherical_bessel_radial", &spherical_bessel_radial);
     m.method("determine_basis", &determine_basis);
+    
+    m.method("SimpleProdBasis_evaluate", &SimpleProdBasis_evaluate);
 }
