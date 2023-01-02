2d056eae203a61e938a45454a0cfa38e3ab987b9
Chuck Witt <witt.chuck@gmail.com>
2023-01-02 21:57:35 +0000

    delete unused version.

--- source/acecore/simpleprodbasis.cpp
+++ source/acecore/simpleprodbasis.cpp
@@ -13,18 +13,3 @@ std::vector<std::complex<double>> SimpleProdBasis_evaluate(
     }
     return AA;
 }
-
-//std::vector<std::complex<double>> SimpleProdBasis_evaluate(
-//    std::vector<std::vector<int>> specm,
-//    std::vector<int> orders,
-//    std::vector<double> A)
-//{
-//    std::vector<std::complex<double>> AA(specm.size());
-//    for (int i=0; i<specm.size(); ++i) {
-//        AA[i] = A[specm[i][0]];
-//        for (int j=1; j<=orders[i]; ++j) {
-//            AA[i] *= A[specm[i][j]];
-//        }
-//    }
-//    return AA;
-//}
--- source/acecore/simpleprodbasis.hpp
+++ source/acecore/simpleprodbasis.hpp
@@ -4,8 +4,3 @@
 std::vector<std::complex<double>> SimpleProdBasis_evaluate(
     std::vector<std::vector<int>> specv,
     std::vector<std::complex<double>> A);
-
-//std::vector<std::complex<double>> SimpleProdBasis_evaluate(
-//    std::vector<std::vector<int>> specm,
-//    std::vector<std::vector<int>> orders,
-//    std::vector<double> A);
