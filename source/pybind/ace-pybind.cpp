66a559faab8844e1acaaec939418f7af0a4dfac5
Chuck Witt <witt.chuck@gmail.com>
2023-01-05 00:51:11 +0000

    add python bindings for latest.

--- source/pybind/ace-pybind.cpp
+++ source/pybind/ace-pybind.cpp
@@ -2,14 +2,16 @@
 #include <pybind11/complex.h>
 #include <pybind11/stl.h>
 
+#include "partitions.hpp"
 #include "spherical_bessel.hpp"
 #include "spherical_harmonic.hpp"
 
 PYBIND11_MODULE(ace, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
+    m.def("determine_basis", &determine_basis, "Docstring.");
     m.def("naive_sph_harm", &naive_sph_harm, "A naive spherical harmonic.");
     m.def("naive_sph_harm_xyz", &naive_sph_harm_xyz, "A naive spherical harmonic.");
+    m.def("partitions", &partitions);
     m.def("spherical_bessel_radial", &spherical_bessel_radial, "Docstring.");
-    m.def("determine_basis", &determine_basis, "Docstring.");
 }
