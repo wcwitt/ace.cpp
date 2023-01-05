5bde51da3ffd38e30edfd22d18d905f771b1b700
Chuck Witt <witt.chuck@gmail.com>
2023-01-05 00:50:40 +0000

    add julia wrapper for latest.

--- source/cxxwrap/ace-cxxwrap.cpp
+++ source/cxxwrap/ace-cxxwrap.cpp
@@ -1,10 +1,11 @@
 #include "jlcxx/jlcxx.hpp"
 #include "jlcxx/stl.hpp"
 
+#include "acecore/simpleprodbasis.hpp"
+#include "acecore/sparsesymmprod.hpp"
+#include "partitions.hpp"
 #include "spherical_harmonic.hpp"
 #include "spherical_bessel.hpp"
-
-#include "acecore/simpleprodbasis.hpp"
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& m)
 {
@@ -12,6 +13,8 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& m)
     m.method("naive_sph_harm_xyz", &naive_sph_harm_xyz);
     m.method("spherical_bessel_radial", &spherical_bessel_radial);
     m.method("determine_basis", &determine_basis);
+    m.method("partitions", &partitions);
     
     m.method("SimpleProdBasis_evaluate", &SimpleProdBasis_evaluate);
+    m.method("SparseSymmProd_evaluate", &SparseSymmProd_evaluate);
 }
